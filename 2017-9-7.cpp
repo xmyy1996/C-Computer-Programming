3c2a9ebdedc450cf4acccb4a3d577d4f0c478953
xmyy1996 <31702487+xmyy1996@users.noreply.github.com>
2017-09-07 13:38:28 +0800

    Update 2017-9-7.cpp

--- 2017-9-7.cpp
+++ 2017-9-7.cpp
@@ -4,14 +4,14 @@ void main()
 {
 	int i = 5;
 	int a = i++;
-	cout<<a<<endl;
-	cout<<i<<endl;
+	cout<<a<<endl;		//输出结果为5
+	cout<<i<<endl;		//输出结果为6
 	i = 5;
 	a = ++i;
-	cout<<a<<endl;
-	cout<<i<<endl;
+	cout<<a<<endl;		//输出结果为6
+	cout<<i<<endl;		//输出结果为6
 
 	i = 5;
-	cout<<i<<endl<<i++<<endl;
+	cout<<i<<endl<<i++<<endl;		//输出结果分别为6和5
 
 }
