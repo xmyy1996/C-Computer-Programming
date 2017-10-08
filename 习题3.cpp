9d42dfb87dfdf5facb4936910be5a90d910116fe
xmyy1996 <31702487+xmyy1996@users.noreply.github.com>
2017-10-08 15:10:26 +0800

    习题3
    习题3

--- 习题3.cpp
+++ 习题3.cpp
@@ -34,7 +34,6 @@ void main()
 	}
 }		//switch实现
 
-
 # include <iostream.h>
 
 void main()
@@ -69,3 +68,108 @@ void main()
 /////////
 //第3题//
 /////////
+# include <iostream.h>
+
+void main()
+{
+	int i, j;
+	cout<<"请输入一个整数：";
+	cin>>i;
+	j = i % 7;
+	if (j == 0)
+	{
+		cout<<"YES"<<endl;
+	}
+	else
+	{
+		cout<<"NO"<<endl;
+	}
+}
+//运行结果：
+//请输入一个整数：14
+//YES
+//Press any key to continue
+
+/////////
+//第5题//
+/////////
+# include <iostream.h>
+# include <math.h>
+
+void main()
+{
+	float a, b, c;
+	float area, s;
+	cout<<"请输入三边的值a, b, c:";
+	cin>>a>>b>>c;
+	s = (a + b + c)/2;
+	area = sqrt(s * (s - a)*(s - b)*(s - c));
+	cout<<"三角形面积S="<<area<<endl;
+}
+//运行结果：
+//请输入三边的值a, b, c:2.5 3.6 4.7
+//三角形面积S=4.44203
+//Press any key to continue
+
+/////////
+//第6题//
+/////////
+# include <iostream.h>
+
+void main()
+{
+	float r, pi, area, c;
+	int k;
+	pi = 3.1415926;
+	cout<<"计算圆的面积请输入1"<<endl<<"计算圆的周长请输入2"<<endl<<"计算圆的面积和周长请输入3"<<endl;
+	cin>>k;
+	cout<<"请输入圆的半径："<<endl;
+	cin>>r;
+	area = r * r * pi;
+	c = 2 * r * pi;
+	switch (k)
+	{
+		case 1: cout<<"圆的面积S = "<<area<<endl;
+			break;
+		case 2: cout<<"圆的周长C = "<<c<<endl;
+			break;
+		default: cout<<"圆的面积S = "<<area<<"圆的周长C = "<<c<<endl;
+	}
+}
+//运行结果：
+//计算圆的面积请输入1
+//计算圆的周长请输入2
+//计算圆的面积和周长请输入3
+//3
+//请输入圆的半径：
+//6
+//圆的面积S = 113.097圆的周长C = 37.6991
+//Press any key to continue
+
+/////////
+//第8题//
+/////////
+# include <iostream.h>
+
+void main()
+{
+	int a, b, m, n;
+	cout<<"请输入整数a, b:"<<endl;
+	cin>>a>>b;
+	m = a / b;
+	n = a % b;
+
+	if (n ==0)
+	{
+		cout<<a<<'/'<<b<<'='<<m<<endl;
+	}
+	else
+	{
+		cout<<a<<'/'<<b<<'='<<m<<"…"<<n<<endl;
+	}
+}
+//运行结果：
+//请输入整数a, b:
+//148 3
+//148/3=49…1
+//Press any key to continue
